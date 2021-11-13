947fffe3959c5897067eb7227321f6edfe37cc0d
J B <jeanbaptistevanparysgmail.com>
2021-11-13 20:56:48 +0100

    messages

--- components/MessageHeader.tsx
+++ components/MessageHeader.tsx
@@ -3,9 +3,9 @@ import FinnanceLogo from "../images/finance.svg";
 
 const MessageHeader = ({ text }) => {
 	return (
-		<div className="flex items-center border-solid border-2 border-gray-600 bg-gray-600 w-full mr-auto ml-auto rounded-xl mt-2 text-3xl p-2">
+		<div className="flex items-center border-solid border-4 border-gray-600 bg-gray-600 w-full mr-auto ml-auto mt-2 text-3xl p-4 pl-12 rounded-3xl">
         <Image src={FinnanceLogo} alt="finance" width={60} height={60} />
-			<h1 className="ml-2 text-white">{text}</h1>
+			<h1 className="ml-4  text-white">{text}</h1>
 		</div>
 	);
 };
--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import ArowLogo from "../images/arrow.svg"
+
 const MessageInput = () => {
     return (
         <form action="">
-            <input type="text" />
-            <input type="submit" />
+            <div className="flex">
+                <input className="p-2 rounded-3xl border-gray-600 bg-gray-600 "  type="text" />
+                <Link href="#">
+                    <Image
+                        src={ArowLogo}
+                        alt="default user"
+                        width={40}
+                        height={40}
+                    />
+                </Link>
+            </div>
+                
         </form>
     );
   };
