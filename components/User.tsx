4e8edf24f26717e07b9b991419a6fb44fe99429c
Jari Valentine <jarivalentine@gmail.com>
2021-11-13 20:41:01 +0100

    header

--- components/User.tsx
+++ components/User.tsx
@@ -3,15 +3,15 @@ import UserLogo from '../images/user.svg'
 
 const User = ({ name, familyname }) => {
 	return (
-            <div className="w-full flex border-b border-blue-300 pb-6 mb-10">
-                <Image
-                    src={UserLogo}
-                    alt="default user"
-                    width={75}
-                    height={75}
-                />
-                <p className="ml-8 text-3xl text-white"><span className="block">{name}</span>{familyname}</p>
-            </div>
+        <div className="w-full flex border-b border-blue-300 pb-6 mb-10">
+            <Image
+                src={UserLogo}
+                alt="default user"
+                width={75}
+                height={75}
+            />
+            <p className="ml-8 text-3xl text-white"><span className="block">{name}</span>{familyname}</p>
+        </div>
 	);
 };
 
