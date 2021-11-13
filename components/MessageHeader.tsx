f0decd0fb5e4c519eab88dafaa3243cef6f30f35
J B <jeanbaptistevanparysgmail.com>
2021-11-13 18:38:43 +0100

    message dingen

--- components/MessageHeader.tsx
+++ components/MessageHeader.tsx
@@ -1,9 +1,15 @@
-const MessageHeader = ({text}) => {
-  return (
-    <div>
-        <h1>{text}</h1>
-    </div>
-  );
+import Image from "next/image";
+import FinnanceLogo from "../images/finance.svg";
+
+const MessageHeader = ({ text }) => {
+	return (
+		<div className="flex border-solid border-2 border-gray-800 bg-gray-800 w-9/12 m-auto rounded-xl  mt-2 text-3xl ">
+			<div className="ml-2">
+        <Image src={FinnanceLogo} alt="finance" width={60} height={60} />
+      </div>
+			<h1 className="ml-2 text-white">{text}</h1>
+		</div>
+	);
 };
 
 export default MessageHeader;
--- components/Messages.tsx
+++ components/Messages.tsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 const Messages = () => {
     return (
       <div>
-          <Message text="Lorem ipsum bullshit"></Message>
+          <Message text="Heel lekkere text enz"></Message>
       </div>
     );
   };
