228889a1f8ba23e9085f1a7cafc4f9c95828a8ba
J B <jeanbaptistevanparysgmail.com>
2021-11-13 20:40:40 +0100

    svg

--- components/MessageHeader.tsx
+++ components/MessageHeader.tsx
@@ -3,10 +3,8 @@ import FinnanceLogo from "../images/finance.svg";
 
 const MessageHeader = ({ text }) => {
 	return (
-		<div className="flex border-solid border-2 border-gray-800 bg-gray-800 w-9/12 m-auto rounded-xl  mt-2 text-3xl ">
-			<div className="ml-2">
+		<div className="flex items-center border-solid border-2 border-gray-600 bg-gray-600 w-full mr-auto ml-auto rounded-xl mt-2 text-3xl p-2">
         <Image src={FinnanceLogo} alt="finance" width={60} height={60} />
-      </div>
 			<h1 className="ml-2 text-white">{text}</h1>
 		</div>
 	);
