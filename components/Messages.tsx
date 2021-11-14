670538fa8770440552a426cd45c1cae950eedc26
Arthurdw <mail@arthurdw.com>
2021-11-14 10:57:00 +0100

    :sparkles: Index key for message

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -4,20 +4,27 @@ import {
   OutgoingFile,
   OutgoingMessage,
 } from "./Message";
+
 import { Message } from "@hecate-org/blingaton-types/build";
 
 const Messages = (props) => {
   console.log(props);
   return (
-    <div className="h-full py-4  overflow-y-scroll ">
+    <div className="h-full py-4 overflow-y-scroll ">
       <IncommingMessage text="Heel lekkere text enz"></IncommingMessage>
       <OutgoingMessage text="Heel lekkere text enz"></OutgoingMessage>
       <IncommingFile text="Heel lekkere text enz"></IncommingFile>
       <OutgoingFile text="Heel lekkere text enz"></OutgoingFile>
-      {props.messages.map((message: Message) => {
-        console.log(message)
-        return <OutgoingMessage text={message.content}></OutgoingMessage>;
-      })}
+      {props.messages &&
+        props.messages.map((message: Message, index: number) => {
+          console.log(message);
+          return (
+            <OutgoingMessage
+              key={index}
+              text={message.content}
+            ></OutgoingMessage>
+          );
+        })}
     </div>
   );
 };
