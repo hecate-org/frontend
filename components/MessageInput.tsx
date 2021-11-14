f258565b8e12b081ac12258cf8e16eab4ef3a2dc
J B <jeanbaptistevanparysgmail.com>
2021-11-14 11:36:53 +0100

    file upload

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -2,19 +2,33 @@ import Link from "next/link";
 import Image from "next/image";
 import ArowLogo from "../images/send.svg";
 import Upload from "../images/upload.svg";
-
-const upload = () => { 
-    document.getElementById("ref").click();
- }
+import { useState } from "react";
 
 const MessageInput = () => {
+	const [image, setImage] = useState(null);
+
+	const upload = () => {
+		document.getElementById("ref").click();
+	};
+
+	const uploadToServer = async (event) => {
+		const body = new FormData();
+		body.append("file", image);
+		const response = await fetch("/api/file", {
+			method: "POST",
+			body,
+		});
+	};
 	return (
 		<form action="#">
 			<div className="flex items-center">
-				<button onClick={upload} className="border-gray-600 border-1 p-3  bg-gray-600 rounded-xl mr-2">
+				<button
+					onClick={upload}
+					className="border-gray-600 border-1 p-3  bg-gray-600 rounded-xl mr-2"
+				>
 					<Image src={Upload} alt="upload" width={20} height={20} />
 				</button>
-				<input id="ref" hidden type="file"/>
+				<input id="ref" hidden type="file" />
 				<input
 					className="p-2 rounded-xl text-2xl text-white border-gray-600 bg-gray-600 w-full focus:border-gray-500 pl-4"
 					placeholder="Write a message ..."
