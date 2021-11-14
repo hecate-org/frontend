5bdf033685dff888cec560b006dd7627c5954928
J B <jeanbaptistevanparysgmail.com>
2021-11-14 01:49:52 +0100

    files

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,28 +1,65 @@
 import Image from "next/image";
 import FinnanceLogo from "../images/finance.svg";
+import DownloadLogo from "../images/download.svg";
 
+const IncommingMessage = ({ text }) => {
+	return (
+		<div className="bg-gray-600 text-white flex p-3 rounded-r-2xl rounded-b-2xl max-w-md my-4 shadow-xl">
+			<div className="px-5 w-24">
+				<Image src={FinnanceLogo} alt="finance" width={35} height={35} />
+			</div>
+			<div className="w-full">{text}</div>
+			<div className="px-5 text-gray-400">12:00</div>
+		</div>
+	);
+};
 
+export { IncommingMessage };
 
-const IncommingMessage = ({text}) => {
-    return (
-      <div className="bg-gray-600 text-white flex p-3 rounded-r-2xl rounded-b-2xl max-w-md my-4 shadow-xl">
-          <div className="px-5 w-24"><Image src={FinnanceLogo} alt="finance" width={35} height={35} /></div>
-          <div className="w-full">{text}</div>
-          <div className="px-5 text-gray-400">12:00</div>
-      </div>
-    );
-  };
-  
-  export {IncommingMessage};
+const OutgoingMessage = ({ text }) => {
+	return (
+		<div className="bg-gray-600 text-white flex p-3 rounded-l-2xl rounded-b-2xl max-w-md my-4 shadow-xl ml-auto">
+			<div className="px-5 text-gray-400">12:00</div>
+			<div className="w-full">{text}</div>
+			<div className="px-5 w-24">
+				<Image src={FinnanceLogo} alt="finance" width={35} height={35} />
+			</div>
+		</div>
+	);
+};
 
-  const OutgoingMessage = ({text}) => {
-    return (
-      <div className="bg-gray-600 text-white flex p-3 rounded-l-2xl rounded-b-2xl max-w-md my-4 shadow-xl ml-auto">
-          <div className="px-5 w-24"><Image src={FinnanceLogo} alt="finance" width={35} height={35} /></div>
-          <div className="w-full">{text}</div>
-          <div className="px-5 text-gray-400">12:00</div>
-      </div>
-    );
-  };
-  
-  export {OutgoingMessage};
+export { OutgoingMessage };
+
+const IncommingFile = ({ text }) => {
+	return (
+		<div className="bg-gray-600 text-white flex p-3 rounded-r-2xl rounded-b-2xl max-w-md my-4 shadow-xl">
+			<div className="px-5 w-24">
+				<Image src={FinnanceLogo} alt="finance" width={35} height={35} />
+			</div>
+			<div className="pr-5 w-24">
+				<Image src={DownloadLogo} alt="finance" width={35} height={35} />
+			</div>
+			<div className="w-full">{text}</div>
+			<div className="px-5 text-gray-400">12:00</div>
+		</div>
+	);
+};
+
+export { IncommingFile };
+
+const OutgoingFile = ({ text }) => {
+	return (
+		<div className="bg-gray-600 text-white flex p-3 rounded-l-2xl rounded-b-2xl max-w-md my-4 shadow-xl ml-auto">
+			<div className="px-5 text-gray-400">12:00</div>
+			<div className="w-full">{text}</div>
+			<div className="pl-5 w-24">
+				<Image src={DownloadLogo} alt="finance" width={35} height={35} />
+			</div>
+			<div className="px-5 w-24">
+				<Image src={FinnanceLogo} alt="finance" width={35} height={35} />
+			</div>
+		</div>
+	);
+};
+
+export { OutgoingFile };
--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,10 +1,12 @@
-import {IncommingMessage,OutgoingMessage} from "./Message";
+import {IncommingFile, IncommingMessage,OutgoingFile,OutgoingMessage} from "./Message";
 
 const Messages = () => {
     return (
       <div className="h-full py-4">
           <IncommingMessage text="Heel lekkere text enz"></IncommingMessage>
           <OutgoingMessage text="Heel lekkere text enz"></OutgoingMessage>
+          <IncommingFile text="Heel lekkere text enz"></IncommingFile>
+          <OutgoingFile text="Heel lekkere text enz"></OutgoingFile>
       </div>
     );
   };
